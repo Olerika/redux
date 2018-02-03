8f144aee34e7dce5e7498eb9c0f8b24cae39a143
Olerika <olerikario@gmail.com>
2018-02-03 18:39:54 +0200

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -62,4 +62,4 @@ function mapStateToProps({ weather }) {
 
 function mapDispatchToProps(dispatch) { return bindActionCreators({deleteCity}, dispatch) }
 
-export default connect(mapStateToProps, mapDispatchToProps)(WeatherList);
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherList); //connect to context
